4d734bdb383b2009f0c9050fae7e3a3ab0b9f99c
Naresh Saini <yogeshsainins100@gmail.com>
2026-01-02 19:27:18 +0530

    add pages, UI, folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import  { AppLayout } from "./components/Layout/AppLayout.jsx"
+import "./App.css"
+import { Home } from './pages/Home.jsx'
+import { About } from './pages/About.jsx'
+import { Contact }  from './pages/Contact.jsx'
+import { Country } from './pages/Country.jsx'
+import { ErrorPage } from './pages/ErrorPage.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "about",
+    element: <About />
+  },
+  {
+    path: "contact",
+    element: <Contact />
+  },
+  {
+    path: "country",
+    element: <Country />
+  }
+    ]
+  }
+  
+])
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <RouterProvider router = {router} > </RouterProvider>;
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Layout/AppLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../UI/Headers.jsx'
+import Footer from '../UI/Footer.jsx'
+
+const AppLayout = () => {
+  return (
+    <div>
+        <Header />
+
+        <Footer />
+    </div>
+  )
+}
+
+export default AppLayout
